c294e632336243cf4cc61977c9690baf401a0d50
14talesoflucy <maryokololucy18@gmail.com>
2022-09-13 16:03:58 -0700

    functioms

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet -  a function that prints the alphabet
- *in lowercase
- * Return: Always 0 (Sucess)
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
 {
-	char x;
+	int n;
 
-	x = 'a';
-
-	while (x <= 'z')
+	for (n = 'a'; n <= 'z'; n++)
 	{
-		_putchar(x);
-		x++;
+		_putchar(n);
 	}
-
 	_putchar('\n');
 }
-
-
