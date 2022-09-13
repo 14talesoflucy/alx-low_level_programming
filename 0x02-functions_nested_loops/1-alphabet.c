57913abf9a81545c3a263a4c766239309422d4b4
14talesoflucy <maryokololucy18@gmail.com>
2022-09-13 15:13:54 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  a function that prints the alphabet
+ *in lowercase
+ * Return: Always 0 (Sucess)
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x)
+	}
+	_putchat('\n')
+
+
