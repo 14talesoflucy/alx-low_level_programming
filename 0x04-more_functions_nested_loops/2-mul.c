9f1212e9a267b8f3a2c9529abcd66ebf9e51c890
14talesoflucy <maryokololucy18@gmail.com>
2022-09-15 10:54:31 -0700

    betty

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Entry point: funtion that multiply
+ * @a: first input
+ * @b: second input
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
